2254b685fa9edae67409d9d8e78a805804ce19f2
Franwernke <franwernke@usp.br>
2022-05-31 20:46:08 -0300

    Add P in front of every response

--- server/GenericController.py
+++ server/GenericController.py
@@ -14,46 +14,46 @@ class GenericController:
     if command[0] == "new":
       try:
         self.server.createNewUser(command[1], command[2])
-        return "OK"
+        return "P OK"
       except UserAlreadyExists as e:
-        return e.message
+        return "P " + e.message
 
     elif command[0] == "pass":
       try:
         self.server.changePassword(command[1], command[2], command[3])
-        return "OK"
+        return "P OK"
       except UserNotFoundException as e:
-        return e.message
+        return "P " + e.message
       except WrongPasswordException as e:
-        return e.message
+        return "P " + e.message
 
     elif command[0] == "in":
       try:
         self.server.loginUser(command[1], command[2], (address[0], command[3]))
-        return "OK"
+        return "P OK"
       except UserNotFoundException as e:
-        return e.message
+        return "P " + e.message
       except WrongPasswordException as e:
-        return e.message
+        return "P " + e.message
       except UserAlreadyLoggedIn as e:
-        return e.message
+        return "P " + e.message
       except Exception as e:
         print(e)
 
     elif command[0] == "halloffame":
-      return str(self.server.showHallOfFame())
+      return "P " + str(self.server.showHallOfFame())
 
     elif command[0] == "l":
-      return str(self.server.showOnlinePlayers())
+      return "P " + str(self.server.showOnlinePlayers())
 
     elif command[0] == "call":
       try:
         userSession = self.server.invitePlayer(command[1], command[2])
-        return "OK " + userSession.ip + " " + userSession.port 
+        return "P OK " + userSession.ip + " " + userSession.port 
       except UserIsAlreadyInGame as e:
-        return e.message
+        return "P " + e.message
       except UserIsNotAvailable as e:
-        return e.message
+        return "P " + e.message
       
 
     # elif command[0] == "play":
@@ -70,11 +70,11 @@ class GenericController:
 
     elif command[0] == "out":
       self.server.logout(command[1])
-      return "OK"
+      return "P OK"
     
     elif command[0] == "startgame":
       self.server.startgame(command[1], command[2])
-      return "OK"
+      return "P OK"
 
     elif command[0] == "heartbeat":
       self.server.handleHeartbeat(address)
